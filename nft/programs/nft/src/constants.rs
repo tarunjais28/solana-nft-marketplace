1ef0135a76ce3596eda1b83f0049d97f5a09694f
tarunjais28 <tarunjais28@gmail.com>
2024-06-03 12:50:04 +0530

    Modules for constants

---
+++ nft/programs/nft/src/constants.rs
@@ -0,0 +1,13 @@
+use super::*;
+
+#[constant]
+pub const MINT_TAG: &[u8] = b"mint";
+
+#[constant]
+pub const MAINTAINERS_TAG: &[u8] = b"maintainers";
+
+#[constant]
+pub const COLLECTION_TAG: &[u8] = b"collection";
+
+#[constant]
+pub const COLLECTION_COUNTER_TAG: &[u8] = b"counter";
